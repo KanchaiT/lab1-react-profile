b48e88741c88cdd9acd0d7c9f10fcdaac647619e
Kanchai Triyavanich <kanhrk.08541@gmail.com>
2026-01-05 13:47:17 +0700

    feat: add profileCard component and pass data via props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
+import ProfileCard from "./components/ProfileCard"
+
 function App() {
 
   return (
-    <div>
-        <h1>My First React App</h1>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+        <h1>My Team Portfolio</h1>
+
+        <ProfileCard
+           name = 'Kanchai Triyavanich'
+           role = 'Student @ CEDT'
+           bio = 'Progress, not perfection.'
+        />
+
+        <ProfileCard
+           name = 'Jane Doe'
+           role = 'Guest Developer'
+           bio = 'I love coding and learning new things.'
+        />
     </div>
   )
 }
